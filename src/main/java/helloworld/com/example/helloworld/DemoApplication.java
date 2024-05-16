99ce430e780b99b9508f6b5edbd423f3cb4d8857
christodwise <151842136+christodwise@users.noreply.github.com>
2024-05-16 16:47:25 +0530

    Update DemoApplication.java

--- src/main/java/helloworld/com/example/helloworld/DemoApplication.java
+++ src/main/java/helloworld/com/example/helloworld/DemoApplication.java
@@ -3,15 +3,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class DemoApplication {
 final static Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
+	 @Value("${enable_adminclien}")
+    private String version;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		 
-log.info("logger settings: $enable_adminclient ");
+log.info("logger settings: version);
 	}
 
 }
