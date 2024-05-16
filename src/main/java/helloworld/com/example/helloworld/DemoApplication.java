18ef17eca21d6ce18a91039f8dd324b7c938f9c5
christodwise <151842136+christodwise@users.noreply.github.com>
2024-05-16 16:29:06 +0530

    Update DemoApplication.java

--- src/main/java/helloworld/com/example/helloworld/DemoApplication.java
+++ src/main/java/helloworld/com/example/helloworld/DemoApplication.java
@@ -1,13 +1,16 @@
 package helloworld.com.example.helloworld;
-
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+final static Logger log = LoggerFactory.getLogger(CSApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		 
+log.info("Hi Welcome to Lifetrenz ");
 	}
 
 }
